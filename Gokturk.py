8bbf64d9040d2b59cd5f9fd107a5f8ce241b31b6
hunozkan <132670064+hunozkan@users.noreply.github.com>
2024-08-14 20:40:44 +0300

    Update Gokturk.py

--- Gokturk.py
+++ Gokturk.py
@@ -1,6 +1,9 @@
 meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
+            "ROFL": - "bir şakaya karşılık cevap",
+            "SHEESH": - "onaylamamak"
+            "CREEPY" - "korkunç"
             }
 word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
 
